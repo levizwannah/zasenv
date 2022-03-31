9aac28862d4959ff0386cb6a774ace878d3de9a5
levizwannah <levizwannah@gmail.com>
2022-03-31 20:35:48 +0300

    started working on add interfaces

---
+++ supporters/classes/zas/ifcobject.class.php
@@ -0,0 +1,21 @@
+<?php
+    // Comments with #.# are required by `zas` for code insertion.
+
+    namespace Zas;
+
+    use \Zas\NsUtilTrait;
+    #uns#
+
+
+    class IfcObject extends Transpiler  {
+
+        # use traits
+        use NsUtilTrait;
+        #ut#
+
+        public function __construct(){
+            #code...
+        }
+    }
+
+?>
