69849f482a2c35682a08f3a93e4a5ff1b661bb18
levizwannah <levizwannah@gmail.com>
2022-05-30 04:58:48 +0300

    updated zas-php-cli

--- supporters/classes/custom-zas/cli.class.php
+++ supporters/classes/custom-zas/cli.class.php
@@ -19,7 +19,16 @@
          * @return void
          */
         public function process(int &$argc, array &$argv){
-            parent::process($argc, $argv);
+            $found = parent::process($argc, $argv);
+
+            if($found) return true;
+            # add your commands here in a switch statement
+            # use the ZasConstants to define your constants
+            # show list
+            $mainCommand = strtolower($argv[1]);
+
+            Cli::log("You enter an unknown command");
+            return false;
         }
     }
 
