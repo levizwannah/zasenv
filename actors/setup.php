4d37f4a2f1b6a0bc7413d51095bd64d00042b4d2
levizwannah <levizwannah@gmail.com>
2023-02-08 10:40:16 +0300

    allow setup not to be loaded twice

--- actors/setup.php
+++ actors/setup.php
@@ -3,7 +3,12 @@
  * References the master setup
  * You can add custom setup procedures for actors here.
  */
-
-require(__DIR__. "/../master.setup.php");
-
+(
+    isset($loaded)
+    &&
+    isset($loaded[dirname(__DIR__)])
+)
+or
+require(dirname(__DIR__). "/master.setup.php");
+$loaded[__DIR__] = 1;
 ?>
--- master.setup.php
+++ master.setup.php
@@ -16,6 +16,12 @@ require(__DIR__ . "/auto-loader.class.php");
 */
 $zasConfig = json_decode(file_get_contents(__DIR__ . "/zas-config.json"));
 
+/**
+ * Keeps the hashmap of loaded Setups
+ * @var array<string,int>
+ */
+$loaded = [__DIR__ => 1];
+
 $autoLoader = new AutoLoader();
 $autoLoader->autoLoad();
 ?>
